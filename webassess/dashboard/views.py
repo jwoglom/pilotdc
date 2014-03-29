fb263c82ca74712248c646f636ef7ae39395ed51
nickthename <spamedkid@gmail.com>
2014-03-29 19:18:34 -0400

    Dashboard view updated

--- webassess/dashboard/views.py
+++ webassess/dashboard/views.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+from quest.models import Test
+import datetime
+
 @login_required
 def dashboard_view(request):
-    return render(request, 'dashboard.html', {})
+    test_list = Test.objects.order_by('-postdate')
+    before = [x for x in test_list if x.postdate < datetime.datetime.now]
+    after = [x for x in test_list if x not in before]
+    return render(request, 'dashboard.html', {'before': before, 'after': after})
