49fc5583522c9cc9308de07e5322beae5e499c28
James Woglom <j@wogloms.net>
2014-03-29 16:43:41 -0400

    fix urls

--- webassess/webassess/urls.py
+++ webassess/webassess/urls.py
@@ -10,8 +10,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'webassess.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', dashboard.views.dashboard_view),
-    url(r'^login/$', users.views.process_login, name='process_login'),
-    url(r'^logout/$', users.views.process_logout, name='process_logout'),
-    url(r'^admin/$', users.views.admin_index, name='admin_index'),
+    url(r'^$', 'dashboard.views.dashboard_view', name='dashboard_view'),A
+    url(r'^login/$', 'users.views.process_login', name='process_login'),
+    url(r'^logout/$', 'users.views.process_logout', name='process_logout'),
+    url(r'^admin/$', 'users.views.admin_index', name='admin_index'),
 )
