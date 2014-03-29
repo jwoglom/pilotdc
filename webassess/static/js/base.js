65c43d1163e876c70c71ffc2c87904780037fedc
James Woglom <j@wogloms.net>
2014-03-29 16:19:25 -0400

    first frontend bits

---
+++ webassess/static/js/base.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(".left ul.nav > li[name]").click(function() {
+        nav.go($(this).attr('name'));
+    });
+});
+nav = {
+    go: function(l) {
+        location.href = '#'+l;
+    }
+}
