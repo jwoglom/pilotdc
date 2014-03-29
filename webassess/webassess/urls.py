40da688ad190f2f4481a05f099a1dc9577a6eabb
James Woglom <j@wogloms.net>
2014-03-29 17:18:31 -0400

    quest testtaking page

--- webassess/webassess/urls.py
+++ webassess/webassess/urls.py
@@ -13,11 +13,10 @@ urlpatterns = patterns('',
     url(r'^$', 'dashboard.views.dashboard_view', name='dashboard_view'),
     url(r'^login/$', 'django.contrib.auth.views.login', {
         'template_name': 'login.html',
-        #'name': 'process_login'
     }),
-    #url(r'^login/$', 'users.views.process_login', name='process_login'),
     url(r'^logout/$', 'users.views.process_logout', name='process_logout'),
-    #url(r'^admin/$', 'users.views.admin_index', name='admin_index'),
+
+    url(r'^quest/take/1$', 'quest.views.take_view', name='quest_take'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
