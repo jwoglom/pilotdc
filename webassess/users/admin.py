58aa65a3af9179ec93acbde58393774912f9e0f4
nickthename <spamedkid@gmail.com>
2014-03-29 19:19:18 -0400

    fixed users admin

--- webassess/users/admin.py
+++ webassess/users/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
 from users.models import (
+            School,
             Teacher,
             Student,
             )
 
 admin.site.register(Teacher)
+admin.site.register(School)
 admin.site.register(Student)
 # Register your models here.
