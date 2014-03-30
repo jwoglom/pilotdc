ecfe647da1a0ce416e2acc8e348cc324d7d50cc7
nickthename <spamedkid@gmail.com>
2014-03-29 20:16:45 -0400

    Templates and views and things that go on browsers and stuff

--- webassess/dashboard/views.py
+++ webassess/dashboard/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.utils import timezone 
 
 from quest.models import Test
 import datetime
@@ -7,6 +8,6 @@ import datetime
 @login_required
 def dashboard_view(request):
     test_list = Test.objects.order_by('-postdate')
-    before = [x for x in test_list if x.postdate < datetime.datetime.now]
+    before = [x for x in test_list if x.postdate < timezone.now()]
     after = [x for x in test_list if x not in before]
     return render(request, 'dashboard.html', {'before': before, 'after': after})
--- webassess/quest/views.py
+++ webassess/quest/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+from quest.models import Test
 @login_required
-def take_view(request):
+def take_view(request, number):
     return render(request, 'quest/take.html', {})
