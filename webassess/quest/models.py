be2a7f2788ac56bb8439b08da6ff0ed665b55261
James Woglom <j@wogloms.net>
2014-03-29 21:28:11 -0400

    fix conflict

--- webassess/quest/models.py
+++ webassess/quest/models.py
@@ -16,22 +16,14 @@ class AnswerOption(models.Model):
         return unicode(self.text)
 
 class Question(models.Model):
-<<<<<<< HEAD
     qtype = models.CharField(default="mc", max_length=10)
-=======
-    qtype = models.IntegerField(default=0)
->>>>>>> 24ac23847feae7e7a7d158dfa014c488da691209
     tags = models.ManyToManyField(Tag, related_name='tags')
     #header = models.CharField(default="", max_length=10000)
     header = HTMLField()
     choices = models.ManyToManyField(AnswerOption, related_name='answer_choices')
     answer = models.ForeignKey(AnswerOption, null=True, related_name='answer_correct')
     def __unicode__(self):
-<<<<<<< HEAD
         return unicode("{0} ({1})".format(self.id, self.qtype))
-=======
-        return "{0} ({1}): {2}: {3}".format(self.header, self.qtype, self.choices, self.answer)
->>>>>>> 24ac23847feae7e7a7d158dfa014c488da691209
 
 class Test(models.Model):
 #    num = models.IntegerField(default=0)
@@ -45,13 +37,8 @@ class Test(models.Model):
             blank=True
             )
     questions = models.ManyToManyField(Question)
-<<<<<<< HEAD
-    
+
     def __unicode__(self):
         return unicode(self.num)
-=======
-    def __str__(self):
-        return "Test #" + str(self.id)
->>>>>>> 24ac23847feae7e7a7d158dfa014c488da691209
 
 # Create your models here.
