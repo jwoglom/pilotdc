560b7347d6a9f635ab35d4780489793ec5b01775
James Woglom <j@wogloms.net>
2014-03-29 22:11:02 -0400

    update models

--- webassess/quest/views.py
+++ webassess/quest/views.py
@@ -1,7 +1,32 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from quest.models import AnswerOption, Question, Test, AnswersSave, AnswerSave
+import json
 
-from quest.models import Test
 @login_required
 def take_view(request, number):
     return render(request, 'quest/take.html', {})
+
+"""
+@login_required
+def submit_view(request):
+    def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
+    def json2obj(data): return json.loads(data, object_hook=_json_object_hook)'
+
+    if request.POST:
+        testid = request.POST.get('testid')
+        mapstr = request.POST.get('map')
+        questionstr = request.POST.get('questions')
+        map = json2obj(mapstr)
+        questions = json2obj(questionstr)
+
+        testobj = Test.objects.get(id=testid)
+        save = AnswersSave(
+            test=testobj
+        )
+        save.save()
+        save.saves.add(
+            test=testobj,
+            saves=
+        )
+"""
