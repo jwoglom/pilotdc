180cec7719955ea3c35367578139007a034af699
James Woglom <j@wogloms.net>
2014-03-30 02:49:26 -0400

    update urls

--- webassess/webassess/urls.py
+++ webassess/webassess/urls.py
@@ -13,13 +13,13 @@ urlpatterns = patterns('',
     url(r'^$', 'dashboard.views.dashboard_view', name='dashboard_view'),
     #url(r'^test/<number>/', 'quest.views.test', name='Test interface'),
     url(r'^dashboard/$', 'dashboard.views.dashboard_view', name='dashboard_view'),
-    url(r'^login/$', 'django.contrib.auth.views.login', {
-        'template_name': 'login.html',
-    }),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html',}),
     url(r'^logout/$', 'users.views.process_logout', name='process_logout'),
-
     url(r'^quest/take/(?P<test_id>\d+)$', 'quest.views.take_view', name='quest_take'),
     url(r'^quest/submit/$', 'quest.views.submit_view', name='Submit test'),
+    url(r'^teacher_dashboard/$', 'dashboard.views.teacher_view', name='teacher_dashboard'),
+    url(r'^quest/add/$', 'quest.views.add_view', name='add_view'),
+    url(r'^quest/add/submit/$', 'quest.views.add_submit', name='add_submit'),
     url(r'^admin/', include(admin.site.urls)),
     (r'^tinymce/', include('tinymce.urls')),
 )
