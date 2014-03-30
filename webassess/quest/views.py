b0449f916f4fa036e93f626adbbb76332285c0ac
James Woglom <j@wogloms.net>
2014-03-29 22:20:58 -0400

    separate js file, add err message for no questions

--- webassess/quest/views.py
+++ webassess/quest/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from quest.models import AnswerOption, Question, Test, AnswersSave, AnswerSave
+from quest.models import AnswerOption, Question, Test
+#, AnswersSave, AnswerSave
 import json
 from quest.models import Test
 from django.utils import timezone
